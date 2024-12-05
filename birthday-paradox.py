6836e80a8ba812b617a7e023732186cce3dde57e
mattmcphee <matt.imcphee@gmail.com>
2024-12-05 13:45:23 +1000

    created bagels game and birthday-paradox (wip)

---
+++ birthday-paradox.py
@@ -0,0 +1,13 @@
+"""Birthday Paradox Simulation - Explore the surprising probabilities of the
+'Birthday Paradox'.
+"""
+
+import datetime, random
+
+
+def getBirthdays(numberOfBirthdays):
+    """Returns a list of number random date objects for birthdays.
+
+    Args:
+        numberOfBirthdays (_type_): _description_
+    """
